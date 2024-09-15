0426668151b1c1915d461c1d8a760f23e91a3cd9
Haneen <htsalhagbani@gmail.com>
2024-09-15 13:23:50 +0300

    add logic

--- main.js
+++ main.js
@@ -2,18 +2,17 @@ let url="https://coffee.alexflipnote.dev/random.json"
 let image=document.getElementById('image');
 let continer=document.getElementById('continer');
 
-function update(){
-fetch(url)
+async function  update(){
+ await fetch(url)
 .then((Response)=>Response.json())
 .then((data)=>{
     console.log(data);
     image.src=data.file;
     continer.appendChild(image);
-  
 })
 
 }
 
-setInterval(update, 3000);
+setInterval(update, 5000);
 
 
